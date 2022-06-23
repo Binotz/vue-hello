bb6c722c46384a89a813d9863410673ae9f6ea2c
Binotz <binotto.riccardo@gmail.com>
2022-06-23 15:15:55 +0200

    BONUS done

---
+++ BONUS/js/script.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el:'#root',
+    data:{
+        message: 'Hello VueJS world',
+        image:{
+            source: 'https://miro.medium.com/max/855/1*9A5EFag9rsRWjWswxlZ4hA.png',
+            alt: 'VueJS Logo'
+        }
+    }
+});
